919d5ca413c457a8cf9ba76a059d1c7034e23ee9
Jasson López <jasson_alex99@hotmail.com>
2021-09-10 13:52:56 -0600

    :tada: Init

---
+++ database/migrations/2021_09_10_194734_create_summoners_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSummonersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('summoners', function (Blueprint $table) {
+            $table->id();
+            $table->string('summoner_name');
+            $table->string('twitch_channel');
+            $table->string('main_role');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('summoners');
+    }
+}
