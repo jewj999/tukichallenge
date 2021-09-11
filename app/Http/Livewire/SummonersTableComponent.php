516901a572d76644702304329880835961136d7e
Jasson López <jasson_alex99@hotmail.com>
2021-09-10 21:46:12 -0600

    :sparkles: Ordering

--- app/Http/Livewire/SummonersTableComponent.php
+++ app/Http/Livewire/SummonersTableComponent.php
@@ -21,9 +21,18 @@ class SummonersTableComponent extends Component
     }
     public function render()
     {
-        $summoners = Summoner::with('leagueInfo')->get()->sortByDesc(function (Summoner $summoner) {
-            return Tier::fromKey($summoner->leagueInfo?->tier ?? 'IRON');
-        });
+        $summoners = Summoner::with('leagueInfo')->get()
+            ->sortByDesc(function (Summoner $summoner) {
+                return Tier::fromKey($summoner->leagueInfo?->tier ?? 'IRON');
+            })->groupBy(function (Summoner $summoner) {
+                return $summoner->leagueInfo?->tier ?? 'IRON';
+            })->map(function (Collection $tier) {
+                return $tier->sortByDesc(function (Summoner $summoner) {
+                    return Rank::fromKey($summoner->leagueInfo?->rank ?? 'IIII');
+                });
+            })->collapse();
+
+        // dd($summoners);
         return view('livewire.summoners-table-component', ['summoners' => $summoners]);
     }
 }
