b5912d04e8f1e91f53f6f3b0bfe86ca76d5f203e
Jasson López <jasson_alex99@hotmail.com>
2021-09-10 21:47:34 -0600

    :bug: Summoners ids

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->job(new FetchSummonersIds)->everyMinute();
+        $schedule->job(new FetchSummonersIds)->everyFiveMinutes();
         $schedule->job(new FetchSummonersData)->everyMinute();
     }
 
