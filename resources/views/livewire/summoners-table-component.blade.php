b47ea3e393d7eda8d7c99b8eda1df783738fb2ca
Jasson López <jasson_alex99@hotmail.com>
2021-09-11 16:50:45 -0600

    :fb: logo

--- resources/views/livewire/summoners-table-component.blade.php
+++ resources/views/livewire/summoners-table-component.blade.php
@@ -46,22 +46,34 @@
                 </x-table-cell>
                 <x-table-cell>{{$summoner->summoner_name}}</x-table-cell>
                 <x-table-cell>{{$summoner->level}}</x-table-cell>
-                <x-table-cell class=" text-purple-800 text-center">
+                <x-table-cell
+                    class="{{Str::of($summoner->twitch_channel)->contains('twitch') ? 'text-purple-800'  : 'text-blue-800'}} text-center">
                     <div class="flex justify-center items-center ">
                         @if ($summoner->twitch_stream_status)
-                        <span class="w-2 h-2 rounded-full bg-green-500 mr-2">
+                        <span class="w-2 h-2 rounded-full bg-green-400 mr-2" >
                         </span>
                         @else
-                        <span class="w-2 h-2 rounded-full bg-red-500 mr-2">
+                        <span class="w-2 h-2 rounded-full bg-blue-gray-700 mr-2">
                         </span>
                         @endif
 
+                        @if (Str::of($summoner->twitch_channel)->contains('twitch'))
                         <a href="https://{{$summoner->twitch_channel}}" target="blank">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                                 <path class="fill-current"
                                     d="M2.149 0l-1.612 4.119v16.836h5.731v3.045h3.224l3.045-3.045h4.657l6.269-6.269v-14.686h-21.314zm19.164 13.612l-3.582 3.582h-5.731l-3.045 3.045v-3.045h-4.836v-15.045h17.194v11.463zm-3.582-7.343v6.262h-2.149v-6.262h2.149zm-5.731 0v6.262h-2.149v-6.262h2.149z"
                                     fill-rule="evenodd" clip-rule="evenodd" /></svg>
                         </a>
+                        @else
+                        <a href="">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                                <path class="fill-current"
+                                    d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm3 8h-1.35c-.538 0-.65.221-.65.778v1.222h2l-.209 2h-1.791v7h-3v-7h-2v-2h2v-2.308c0-1.769.931-2.692 3.029-2.692h1.971v3z" />
+                            </svg>
+                        </a>
+                        @endif
+
+
                     </div>
                 </x-table-cell>
                 <x-table-cell>
