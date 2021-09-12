9f8c8190348060306f8adc5ea2be0fdc1b11590c
Jasson López <jasson_alex99@hotmail.com>
2021-09-11 20:16:19 -0600

    spectator every minute

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->job(new FetchSummonersIds)->everyFiveMinutes();
         $schedule->job(new FetchSummonersData)->everyMinute();
-        $schedule->job(new FetchSummonerMatchData)->everyThreeMinutes();
+        $schedule->job(new FetchSummonerMatchData)->everyMinute();
     }
 
     /**
